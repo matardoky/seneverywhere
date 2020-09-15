b0d6a28790627ce0b6b867ad0b1753ebae3f62ce
MatarDoky <mdokydiop@gmail.com>
2020-09-15 15:45:09 +0200

    install django-oscar and config

--- django_react_boilerplate/urls.py
+++ django_react_boilerplate/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.apps import apps
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('', include(apps.get_app_config('oscar').urls[0])),
+
     path('api-auth/', include('rest_framework.urls')),
+    path("api/", include("oscarapi.urls")),
 ]
